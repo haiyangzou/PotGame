d45771b5ec27dcfe510ebb9fbac4b682546dfeff
mac <manhuabar@gmail.com>
2022-02-26 22:31:37 +0800

    add dubbo

--- pot-core/src/main/java/org/pot/core/service/RoomService.java
+++ pot-core/src/main/java/org/pot/core/service/RoomService.java
@@ -1,5 +1,8 @@
 package org.pot.core.service;
 
+import org.apache.dubbo.config.annotation.DubboReference;
+
 public interface RoomService {
+    @DubboReference(loadbalance = "roundrobin")
     String getRoom();
 }
---
+++ pot-room/DockerRun.sh
@@ -0,0 +1,2 @@
+echo docker run
+docker run --privileged=true -d --name pot-room -p 8200:8200 PotRoom
