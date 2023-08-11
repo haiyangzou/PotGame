55d00217aca83bc1d4737b6737b4a6c9617f7b42
zhy <nhisme@126.com>
2023-08-11 19:16:01 +0800

    添加开关功能，报错处理

--- pot-game/src/main/java/org/pot/game/engine/switchcontrol/listener/WorldSwitchListener.java
+++ pot-game/src/main/java/org/pot/game/engine/switchcontrol/listener/WorldSwitchListener.java
@@ -1,11 +1,32 @@
 package org.pot.game.engine.switchcontrol.listener;
 
 import org.pot.game.engine.switchcontrol.ISwitchListener;
+import org.pot.game.engine.switchcontrol.SwitchRecord;
 
 public class WorldSwitchListener implements ISwitchListener {
     private final ISwitchListener listener;
 
     public WorldSwitchListener(ISwitchListener listener) {
         this.listener = listener;
+    }
+
+    @Override
+    public void onPreviewStart(SwitchRecord switchRecord) {
+
+    }
+
+    @Override
+    public void onOpen(SwitchRecord switchRecord) {
+
+    }
+
+    @Override
+    public void onClose(SwitchRecord switchRecord) {
+
+    }
+
+    @Override
+    public void onShowEnd(SwitchRecord switchRecord) {
+
     }
 }
