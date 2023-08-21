857e9decbe21f977295421fe62d6daa8e9bedcb5
zhy <nhisme@126.com>
2023-08-21 18:35:58 +0800

    redisKey

---
+++ pot-dal/src/main/java/org/pot/dal/redis/RedisKey.java
@@ -0,0 +1,28 @@
+package org.pot.dal.redis;
+
+import org.pot.core.util.RedisUtils;
+
+import java.util.Objects;
+
+public enum RedisKey {
+    union("union_snapshot"),
+    union_tech("union_tech_snapshot"),
+    union_trials("union_trials_snapshot"),
+    union_member("union_member_snapshot"),
+    union_member_ids("union_member_ids_snapshot"),
+    union_activity_data("union_activity_data_snapshot"),
+    ;
+    private final String prefix;
+
+    RedisKey(String prefix) {
+        this.prefix = Objects.requireNonNull(prefix);
+    }
+
+    public String build(Object... args) {
+        return RedisUtils.buildRedisKey(this.prefix, args);
+    }
+
+    public String pattern() {
+        return this.prefix + "*";
+    }
+}
